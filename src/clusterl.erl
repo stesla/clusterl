d1637c923b5c80bc399be5c78f497ad8269a281e
Samuel Tesla <samuel.tesla@gmail.com>
2008-12-09 18:18:01 -0600

    Removed template boilerplate.

--- src/clusterl.erl
+++ src/clusterl.erl
@@ -1,20 +1,16 @@
-%%%-------------------------------------------------------------------
-%%% File    : clusterl.erl
-%%% Author  : Samuel Tesla <samuel@alieniloquent.com>
-%%% Description : 
-%%%
-%%% Created :  3 Dec 2008 by Samuel Tesla <samuel@alieniloquent.com>
-%%%-------------------------------------------------------------------
 -module(clusterl).
 
 -behaviour(application).
 
-%% Public API
+%% API
 -export([start/0, stop/0]).
 
 %% Application callbacks
 -export([start/2, stop/1]).
 
+%%====================================================================
+%% API functions
+%%====================================================================
 start() ->
   application:start(?MODULE).
 
@@ -24,25 +20,9 @@ stop() ->
 %%====================================================================
 %% Application callbacks
 %%====================================================================
-%%--------------------------------------------------------------------
-%% Function: start(Type, StartArgs) -> {ok, Pid} |
-%%                                     {ok, Pid, State} |
-%%                                     {error, Reason}
-%% Description: This function is called whenever an application 
-%% is started using application:start/1,2, and should start the processes
-%% of the application. If the application is structured according to the
-%% OTP design principles as a supervision tree, this means starting the
-%% top supervisor of the tree.
-%%--------------------------------------------------------------------
 start(_Type, _StartArgs) ->
   clusterl_sup:start_link().
 
-%%--------------------------------------------------------------------
-%% Function: stop(State) -> void()
-%% Description: This function is called whenever an application
-%% has stopped. It is intended to be the opposite of Module:start/2 and
-%% should do any necessary cleaning up. The return value is ignored. 
-%%--------------------------------------------------------------------
 stop(_State) ->
   ok.
 
--- src/clusterl_sup.erl
+++ src/clusterl_sup.erl
@@ -1,10 +1,3 @@
-%%%-------------------------------------------------------------------
-%%% File    : clusterl_sup.erl
-%%% Author  : Samuel Tesla <samuel@alieniloquent.com>
-%%% Description :
-%%%
-%%% Created :  3 Dec 2008 by Samuel Tesla <samuel@alieniloquent.com>
-%%%-------------------------------------------------------------------
 -module(clusterl_sup).
 
 -behaviour(supervisor).
@@ -20,25 +13,12 @@
 %%====================================================================
 %% API functions
 %%====================================================================
-%%--------------------------------------------------------------------
-%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
-%% Description: Starts the supervisor
-%%--------------------------------------------------------------------
 start_link() ->
   supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 %%====================================================================
 %% Supervisor callbacks
 %%====================================================================
-%%--------------------------------------------------------------------
-%% Func: init(Args) -> {ok,  {SupFlags,  [ChildSpec]}} |
-%%                     ignore                          |
-%%                     {error, Reason}
-%% Description: Whenever a supervisor is started using
-%% supervisor:start_link/[2,3], this function is called by the new process
-%% to find out about restart strategy, maximum restart frequency and child
-%% specifications.
-%%--------------------------------------------------------------------
 init([]) ->
   Network = {clusterl_network,
              {clusterl_network, start_link, []},
