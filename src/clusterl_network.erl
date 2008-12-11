c50bb0b40432a3e94aa2d0287b71d790e328059c
Samuel Tesla <samuel.tesla@gmail.com>
2008-12-10 22:59:28 -0600

    Switched network over to an FSM.

--- src/clusterl_network.erl
+++ src/clusterl_network.erl
@@ -1,13 +1,18 @@
 -module(clusterl_network).
 
--behaviour(gen_server).
+-behaviour(gen_fsm).
 
 %% API
 -export([start_link/1]).
 
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+%% gen_fsm callbacks
+-export([init/1, handle_event/3,
+         handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+
+%% state_name/2, state_name/3
+%% disconnected
+%% connected
+%% full
 
 -include("protocol.hrl").
 
@@ -15,16 +20,14 @@
                 port,
                 socket}).
 
--define(SERVER, {local, ?MODULE}).
-
 %%====================================================================
 %% API
 %%====================================================================
 start_link(Id) ->
-  gen_server:start_link(?SERVER, ?MODULE, [Id], []).
+  gen_fsm:start_link({local, ?MODULE}, ?MODULE, [Id], []).
 
 %%====================================================================
-%% gen_server callbacks
+%% gen_fsm callbacks
 %%====================================================================
 init([Id]) ->
   case open_socket() of
@@ -33,36 +36,43 @@ init([Id]) ->
       error_logger:info_msg("Network Port = ~p~n", [Port]),
       clusterl_radio:add_listener(self()),
       clusterl_radio:broadcast(?ANNOUNCE(Id, Port)),
-      {ok, #state{id=Id, port=Port, socket=Socket}};
+      {ok, disconnected, #state{id=Id, port=Port, socket=Socket}};
     Error ->
       {stop, Error}
   end.
 
-handle_call(_Request, _From, State) ->
+%%% state_name(_Event, State) ->
+%%%   {next_state, state_name, State}.
+
+%%% state_name(_Event, _From, State) ->
+%%%   Reply = ok,
+%%%   {reply, Reply, state_name, State}.
+
+handle_event(_Event, StateName, State) ->
+  {next_state, StateName, State}.
+
+handle_sync_event(_Event, _From, StateName, State) ->
   Reply = ok,
-  {reply, Reply, State}.
+  {reply, Reply, StateName, State}.
 
-handle_cast(_Msg, State) ->
-  {noreply, State}.
+handle_info({radio_signal, Ip, UdpPort, Signal}, StateName, State) ->
+  handle_signal(Ip, UdpPort, Signal, StateName, State),
+  {next_state, StateName, State};
 
-handle_info({radio_signal, Ip, UdpPort, Signal}, State) ->
-  handle_signal(Ip, UdpPort, Signal, State),
-  {noreply, State};
-
-handle_info(Info, State) ->
+handle_info(Info, StateName, State) ->
   error_logger:info_msg("Received Message: ~p~n", [Info]),
-  {noreply, State}.
+  {next_state, StateName, State}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, _StateName, _State) ->
   ok.
 
-code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+code_change(_OldVsn, StateName, State, _Extra) ->
+  {ok, StateName, State}.
 
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
-handle_signal(_Ip, _UdpPort, ?ANNOUNCE(Id, TcpPort), State) ->
+handle_signal(_Ip, _UdpPort, ?ANNOUNCE(Id, TcpPort), _StateName, State) ->
   #state{id=MyId, port=_MyTcpPort} = State,
   case Id of
     MyId -> ignore;
@@ -70,7 +80,7 @@ handle_signal(_Ip, _UdpPort, ?ANNOUNCE(Id, TcpPort), State) ->
       error_logger:info_msg("ANNOUNCE ~p ~p~n", [Id, TcpPort])
   end;
 
-handle_signal(Ip, UdpPort, Signal, _State) ->
+handle_signal(Ip, UdpPort, Signal, _StateName, _State) ->
   {Q1, Q2, Q3, Q4} = Ip,
   error_logger:info_msg("Signal from ~B.~B.~B.~B:~B -- ~p~n",
                         [Q1, Q2, Q3, Q4, UdpPort, Signal]).
